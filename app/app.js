31d8cc4e2e4895fba883a264bd1bd9a5b12f7887
sanghoon.lee <shfd91@gmail.com>
2023-01-18 23:20:04 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -5,10 +5,9 @@ const express = require('express');
 const app = express();
 // 환경변수 관리 모듈
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
 dotenv.config();
-app.use(express.json());
-// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 제대로 인식되지 않는 문제 해결
-app.use(express.urlencoded({extended:true}));
 
 // routing
 const home = require('./src/routes/home');
@@ -19,6 +18,10 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 제대로 인식되지 않는 문제 해결
+app.use(express.urlencoded({extended:true}));
+app.use(morgan("dev"));
 app.use('/', home); // use -> 미들웨어 등록해주는 메서드
 
 module.exports = {
--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const app = require('../app');
+const logger = require("../src/config/logger");
 const port = process.env.PORT || 3000;
 
 app.app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  logger.info(`Example Server listening on port ${port}`)
 });
